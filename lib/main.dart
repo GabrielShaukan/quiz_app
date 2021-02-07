2add7ef8bdb8bfdcc798be36732dfdfd850abdbe
GabrielShaukan <gabrielshaukan@yahoo.com>
2021-02-07 12:57:12 +0100

    Seperated question into a new widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import './question.dart';
+
 void main() {
   runApp(MyApp());
 }
@@ -7,19 +9,18 @@ void main() {
 class MyApp extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
-    // TODO: implement createState
-    return MyAppState();
+    return _MyAppState();
   }
 }
 
-class MyAppState extends State<MyApp> {
-  var questionIndex = 0;
+class _MyAppState extends State<MyApp> {
+  var _questionIndex = 0;
 
-  void answerQuestion() {
+  void _answerQuestion() {
     setState(() {
-      questionIndex++;
+      _questionIndex++;
     });
-    print(questionIndex);
+    print(_questionIndex);
   }
 
   @override
@@ -36,8 +37,8 @@ class MyAppState extends State<MyApp> {
         ),
         body: Column(
           children: [
-            Text(questions[questionIndex]),
-            RaisedButton(child: Text('question 1'), onPressed: answerQuestion),
+            Question(questions[_questionIndex]),
+            RaisedButton(child: Text('question 1'), onPressed: _answerQuestion),
             RaisedButton(
                 child: Text('question 2'),
                 onPressed: () => print('Answer 2 chosen!')),
